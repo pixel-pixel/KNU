08659cbef98de4991983a6cfbf27120e56815638
pixel-pixel <bartishco@gmail.com>
2021-11-02 10:37:41 +0200

    [edited] RUBY modul1 (task2)

--- SEMESTER_5/RUBY/modul1/task2.rb
+++ SEMESTER_5/RUBY/modul1/task2.rb
@@ -2,11 +2,13 @@ class Book
   @@count = 0
   @tune = nil
 
+  attr_reader :id, :name, :year, :izd, :author, :prise, :pages, :type
+
   def initialize(*params)
     case params.size
-    when 7
+    when 8
       full_constructor(*params)
-    when 6
+    when 7
       constructor_without_id(*params)
     when 3
       name_constructor(*params)
@@ -78,8 +80,13 @@ class Book
   end
 end
 
-
 arr = Book.random_array(5)
-puts arr
-arr.filter {|el| }
+arr.push Book.new("book1", "Author1995", "123", 1900, 100, 200, "papper")
+arr.push Book.new("book2", "Author1994", "321", 1950, 150, 200, "pereplet")
+arr.push Book.new("book3", "Author1996", "123", 1100, 1000, 20, "papper")
+arr.push Book.new("book4", "Author1995", "0", 1950, 10, 790, "papper")
+
+puts "Author1995:\n #{arr.filter{|el| el.author == "Author1995"}.map{ |el| el.to_s }} \n\n"
+puts "izd 123:\n #{arr.filter{|el| el.izd == "123"}.map{ |el| el.to_s }} \n\n"
+puts "After 1940 year:\n #{arr.filter{|el| el.year > 1940 }.map{ |el| el.to_s }} \n\n"
 
